caf5f1f16b70cb6a2455fd5bb9fb2dcb426dd378
João Victor dos Santos Peixoto <100140209+JoaoVictorSPeixoto@users.noreply.github.com>
2022-02-23 11:13:10 -0300

    novo script

--- Script.js
+++ Script.js
@@ -1,46 +1,5 @@
-let cont = 0;
-let contador = document.getElementById('cont')
+const cont = 0;
 
-function Diminuir(){
-    cont -= 1;
+let btns = document.querySelector('.btns')
 
-    contador.innerHTML = cont;
-
-    if(cont < 0 && cont != 0){
-        contador.style.color = 'red'
-    }else{
-        contador.style.color = 'green'
-    }
-    if(cont == 0){
-        contador.style.color = 'black'
-    }
-}
-
-function Resetar(){
-    contador.innerHTML = 0;
-    cont = 0
-    if(cont < 0 && cont != 0){
-        contador.style.color = 'red'
-    }else{
-        contador.style.color = 'green'
-    }
-
-    if(cont == 0){
-        contador.style.color = 'black'
-    }
-}
-
-function Aumentar(){
-    cont += 1;
-
-    contador.innerHTML = cont;
-    if(cont < 0 && cont != 0){
-        contador.style.color = 'red'
-    }else{
-        contador.style.color = 'green'
-    }
-
-    if(cont == 0){
-        contador.style.color = 'black'
-    }
-}
+console.log(btns)
