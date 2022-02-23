a2343737c1b3be50881fdd19fd3dfdae673ff201
João Victor dos Santos Peixoto <100140209+JoaoVictorSPeixoto@users.noreply.github.com>
2022-02-23 11:49:05 -0300

    Script 2.0

--- Script.js
+++ Script.js
@@ -1,5 +1,28 @@
-const cont = 0;
+let cont = 0;
+let contador = document.getElementById('cont')
+let btns = document.querySelectorAll(".btn")
 
-let btns = document.querySelector('.btns')
 
-console.log(btns)
+btns.forEach(function(btn){
+    btn.addEventListener('click',function(botão){
+       if(botão.currentTarget.id == 'Au'){
+           cont++
+           contador.textContent = cont;
+       }else if( botão.currentTarget.id =='Di'){
+           cont--
+           contador.textContent = cont;
+       }else{
+           cont = 0
+           contador.textContent = cont;
+       }
+
+       if(cont > 0){
+           contador.style.color = 'green'
+       }else if(cont < 0){
+        contador.style.color = 'red'
+       }else{
+        contador.style.color = 'black'
+       }
+    })
+})
+
